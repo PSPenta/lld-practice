137808673b86062a045caf7099a3f38864e7e8a4
Akash Mukherjee <akashmukherjee452@gmail.com>
2026-02-01 01:08:59 +0530

    RateLimiter modified

--- RateLimiter2/index.js
+++ RateLimiter2/index.js
@@ -2,6 +2,7 @@ const RateLimiter = require("./RateLimiter");
 const TokenBucket = require("./TokenBucket");
 const FixedWindowCounter = require("./FixedWindowCounter");
 const SlidingWindowLog = require("./SlidingWindowLog");
+const SlidingWindowCounter = require('./SlidingWindowCounter');
 const LeakyBucket = require("./LeakyBucket");
 
 const rateLimiter = new RateLimiter(new TokenBucket(5, 1));
@@ -16,17 +17,23 @@ rateLimiter3.isAllowed("127.0.0.1");
 const rateLimiter4 = new RateLimiter(new LeakyBucket(5, 1));
 rateLimiter4.isAllowed("127.0.0.1");
 
-console.log(rateLimiter.isAllowed("127.0.0.1"));
-console.log(rateLimiter2.isAllowed("127.0.0.1"));
-console.log(rateLimiter3.isAllowed("127.0.0.1"));
-console.log(rateLimiter4.isAllowed("127.0.0.1"));
+const rateLimiter5 = new RateLimiter(new SlidingWindowCounter(5, 1000));
+rateLimiter5.isAllowed('127.0.0.1');
 
 console.log(rateLimiter.isAllowed("127.0.0.1"));
 console.log(rateLimiter2.isAllowed("127.0.0.1"));
 console.log(rateLimiter3.isAllowed("127.0.0.1"));
 console.log(rateLimiter4.isAllowed("127.0.0.1"));
+console.log(rateLimiter5.isAllowed('127.0.0.1'));
 
 console.log(rateLimiter.isAllowed("127.0.0.1"));
 console.log(rateLimiter2.isAllowed("127.0.0.1"));
 console.log(rateLimiter3.isAllowed("127.0.0.1"));
 console.log(rateLimiter4.isAllowed("127.0.0.1"));
+console.log(rateLimiter5.isAllowed('127.0.0.1'));
+
+console.log(rateLimiter.isAllowed("127.0.0.1"));
+console.log(rateLimiter2.isAllowed("127.0.0.1"));
+console.log(rateLimiter3.isAllowed("127.0.0.1"));
+console.log(rateLimiter4.isAllowed("127.0.0.1"));
+console.log(rateLimiter5.isAllowed('127.0.0.1'));
