4e73cd64d3307aedb6dd3308ae71a5333831c669
Akash Mukherjee <akashmukherjee452@gmail.com>
2026-02-04 12:43:03 +0530

    PubSub modified

--- Pub-Sub/pubsubCoreJS.js
+++ Pub-Sub/pubsubCoreJS.js
@@ -18,15 +18,15 @@ const consume = (topic) => {
 
 let data = 0;
 const interval = setInterval(() => {
-	produce("myTopic", data);
-       data += 1;
-	if (data > 10) {
-		clearInterval(interval);
-	}
+  produce('myTopic', data);
+  data += 1;
+  if (data > 10) {
+    clearInterval(interval);
+  }
 }, 1000);
 
 const consumer = consume("myTopic");
 
 consumer.on("data", (data) => {
-	console.log("got data", data);
+  console.log('got data', data);
 });
